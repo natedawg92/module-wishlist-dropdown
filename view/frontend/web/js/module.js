713d68af74f0b5ed0bd9ec97584ae4c5e0c7b108
Nathan Day <nathanday92@gmail.com>
2020-03-01 00:17:48 +0000

    Modify JS To add WIshlist to Mobile Menu

--- view/frontend/web/js/module.js
+++ view/frontend/web/js/module.js
@@ -1,15 +1,9 @@
 define([
     'jquery',
-    'mage/mage',
-    'mage/ie-class-fixer',
     'domReady!'
 ], function($) {
     'use strict';
 
-    $('.wishlist-wrapper')
-        .clone()
-        .appendTo('#store\\.links .header.links')
-        .wrap('<li class="wishlist-wrapper-mobile" />')
-        .contents()
-        .unwrap();
+    $('.wishlist-wrapper').clone().appendTo('#store\\.links').toggleClass('wishlist-wrapper wishlist-wrapper-mobile header links');
 });
+``
